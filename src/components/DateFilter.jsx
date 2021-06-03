042b216244221639f0dda374174b11ddc9299751
Sebastian Casas <sebastian.casas@parqueexplora.org>
2021-06-02 22:11:48 -0500

    Added dates validation and filter by dates

--- src/components/DateFilter.jsx
+++ src/components/DateFilter.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 function DateFilter(props) {
-  return <input type='date' />
+  return (
+    <div className='flex items-center'>
+      <label htmlFor={props.name}>
+        <h3 className='mr-3 font-bold text-yellow-50'>{props.label}</h3>
+      </label>
+      <input className='px-2 py-1 rounded-md bg-yellow-50 font-bold text-blue-900' type='date' name={props.name} value={props.value} onChange={props.onFilterChange} />
+    </div>
+  )
 }
 
 export default DateFilter
